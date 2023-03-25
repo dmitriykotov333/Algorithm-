bed1073028bdbd680478d6d9bb920afedf43479d
Dmitry Kotov <40573791+dmitriykotov333@users.noreply.github.com>
2023-03-26 00:42:11 +0300

    Create SeletionSort.kt

---
+++ SeletionSort.kt
@@ -0,0 +1,55 @@
+enum class Sorted {
+    MAX,
+    MIN
+}
+
+fun ArrayList<Int>.findMax(): Int {
+    var array = this[0]
+    var index = 0
+    for(i in indices) {
+        if (this[i] > array) {
+            array = this[i]
+            index = i
+        }
+    }
+    return index
+}
+
+fun ArrayList<Int>.findMin(): Int {
+    var array = this[0]
+    var index = 0
+    for(i in indices) {
+        if (this[i] < array) {
+            array = this[i]
+            index = i
+        }
+    }
+    return index
+}
+
+fun ArrayList<Int>.sortArray(sorted: Sorted): ArrayList<Int>? {
+    if (isEmpty()) return null
+    val array = arrayListOf<Int>()
+     for(i in indices) {
+        val index = when (sorted) {
+            Sorted.MAX -> {
+                findMax()
+            }
+            Sorted.MIN -> {
+                findMin()
+            }
+        }
+        array.add(
+            this[index]
+        )
+        removeAt(index)
+    }
+    return array
+}
+
+val sortedArray = arrayListOf(333, 22, 12, 22, 11111, 1433, 15, 1022)
+
+
+fun main(args: Array<String>) {
+    print(sortedArray.sortArray(Sorted.MAX))
+}
